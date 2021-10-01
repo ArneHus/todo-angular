65ad571a21b4d6d481644bd277a25feff6f994d1
Arne Hus <r0793284@student.thomasmore.be>
2021-10-01 19:29:25 +0200

    Statische sidebar af

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
@@ -8,7 +8,12 @@ import { map, shareReplay } from 'rxjs/operators';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
+
+  ngAfterViewInit() {
+
+  }
+
   title = 'todo';
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
   .pipe(
@@ -17,4 +22,12 @@ export class AppComponent {
   );
 
   constructor(private breakpointObserver: BreakpointObserver) {}
+
+  toggle_sidebar(){
+    if (document.getElementById('page-wrapper')!.classList.contains("toggled")){
+      document.getElementById('page-wrapper')!.classList.remove("toggled");
+    } else {
+      document.getElementById('page-wrapper')!.classList.add("toggled");
+    }
+  }
 }
