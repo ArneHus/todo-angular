fb37ef41f74a949390162d063fd8a81aeaf832c1
Arne Hus <r0793284@student.thomasmore.be>
2021-10-02 11:34:19 +0200

    Taaklijst en taak toegevoegd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { ListComponent } from './list/list.component';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
+    ListComponent,
+    TaskComponent,
   ],
   imports: [
     BrowserModule,
