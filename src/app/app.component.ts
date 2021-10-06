a90f0193275fee3ded810bf73d2d985f264acddb
Arne Hus <r0793284@student.thomasmore.be>
2021-10-06 13:42:57 +0200

    Lijstjes worden ingeladen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,56 @@
-import { Component, AfterViewInit } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { Component } from '@angular/core';
+import { List } from './list';
+import { CategoryService } from './category.service';
+import { Category } from './category';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit{
-
-  ngAfterViewInit() {
-
-  }
+export class AppComponent {
 
   title = 'todo';
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-  .pipe(
-    map(result => result.matches),
-    shareReplay()
-  );
+  lists: Array<List> = [];
+  categoryName: string = "";
+  categories: Category[] = [];
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private categoryService: CategoryService) {}
+
+  setLists(event: any){
+    this.lists = [];
+    // Zoek in alle categoriën
+    if (event.allCategories){
+      //Alle categoriën ophalen
+      this.categoryService.getCategories().subscribe(result => {
+        this.categories = result
+        this.listCategories();
+      });
+
+      this.categoryName = "Overzicht";
+    } else {
+      //Er wordt maar één lijst doorgegeven
+      if (event.singleList != undefined){
+        this.lists.push(event.singleList);
+        this.categoryName = event.category.name;
+      //Er wordt één categorie doorgegeven
+      } else {
+        event.category.lists.forEach((list: List) => {
+          this.lists.push(list);
+        });
+        this.categoryName = event.category.name;
+      }
+    }
+  }
+
+  listCategories(){
+    //Alle lijsten voor alle categorien in lists steken
+    this.categories.forEach(category => {
+      category.lists.forEach(list => {
+        this.lists.push(list);
+      });
+    });
+  }
 
   toggle_sidebar(){
     if (document.getElementById('page-wrapper')!.classList.contains("toggled")){
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { OnInit, Component, EventEmitter, Output } from '@angular/core';
 import * as $ from "jquery";
 import { Category } from '../category';
 import { CategoryService } from '../category.service';
+import { List } from '../list';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,32 +11,44 @@ import { CategoryService } from '../category.service';
 })
 export class SidebarComponent implements OnInit {
 
-  @Input() category: Category = {name: "", lists: []};
-
   categories: Category[] = [];
 
   constructor(private categoryService: CategoryService) { }
 
   ngOnInit(): void {
     this.categoryService.getCategories().subscribe(result => this.categories = result);
+    this.allLists();
+  }
+
+  @Output()
+  changedList: EventEmitter<any> = new EventEmitter();
+
+  changeList(event: any, category: Category, singleList?: List) {
+    if (event.target!.tagName != 'I'){
+      this.changedList.emit({category, singleList});
+    }
+  }
+
+  allLists() {
+    var allCategories = true;
+    this.changedList.emit({allCategories});
   }
 
   openDropdown(event: any){
-    console.log(event.target.tagName);
     //Checken of er niet geklikt is op een a of ul tag
     if (event.target.tagName != 'A' && event.target.tagName != 'UL'){
       //Checken of er niet geklikt is op een submenuitem
-      if (!event.target.parentNode.parentNode.parentNode.parentNode.classList.contains('sidebar-submenu')){
+      if (!event.target.parentNode.parentNode.parentNode.classList.contains('sidebar-submenu')){
         if (event.target.parentNode.parentNode.classList.contains('active')){
           event.target.parentNode.parentNode.classList.remove('active')
-          $(event.target.parentNode.parentNode.children[1]).slideUp(200);
+          $(event.target.parentNode.parentNode.parentNode.children[1]).slideUp(200);
 
           //Icoontje veranderen van min naar pijltje
           event.target.parentNode.children[1].classList.add('hidden');
           event.target.parentNode.children[0].classList.remove('hidden');
         } else {
           event.target.parentNode.parentNode.classList.add('active')
-          $(event.target.parentNode.parentNode.children[1]).slideDown(200);
+          $(event.target.parentNode.parentNode.parentNode.children[1]).slideDown(200);
 
           //Icoontje veranderen van pijltje naar min
           event.target.parentNode.children[0].classList.add('hidden');
