06af9f2a313add8c14559802727e5bed9898059d
Arne Hus <r0793284@student.thomasmore.be>
2021-11-19 14:30:46 +0100

    Red date when overdue en + fix

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -3,22 +3,30 @@ import { Task } from '../task';
 import { TaskService } from '../task.service';
 import { FormControl, FormGroup } from '@angular/forms';
 import * as moment from 'moment';
-import { DatePipe } from '@angular/common'
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
-  styleUrls: ['./task.component.scss']
+  styleUrls: ['./task.component.scss'],
 })
 export class TaskComponent implements OnInit {
-
   crossed!: {};
-  colorString: string = "";
+  passed!: {};
+  colorString: string = '';
 
   @Input() draggable: boolean = false;
-  @Input() task: Task = { id: 0, task: "", isImportant: false, finishDate: "", finished: false, orderValue: 0, listId: 0 };
+  @Input() task: Task = {
+    id: 0,
+    task: '',
+    isImportant: false,
+    finishDate: '',
+    finished: false,
+    orderValue: 0,
+    listId: 0,
+  };
   @Input() isEditable: boolean = false;
-  @Input() color: string = "";
+  @Input() color: string = '';
 
   @Output()
   newTask: EventEmitter<any> = new EventEmitter();
@@ -29,68 +37,87 @@ export class TaskComponent implements OnInit {
   @Output()
   updatedTaskEvent: EventEmitter<any> = new EventEmitter();
 
-
-  setImportant(){
-    this.task.isImportant = !this.task.isImportant
-    this.taskService.putTask(this.task.id, this.task).subscribe(result => {
+  setImportant() {
+    this.task.isImportant = !this.task.isImportant;
+    this.taskService.putTask(this.task.id, this.task).subscribe((result) => {
       //Task is updated
     });
     this.updatedTaskEvent.emit();
   }
 
-  setFinished(){
-    this.task.finished = !this.task.finished
-    this.taskService.putTask(this.task.id, this.task).subscribe(result => {
+  setFinished() {
+    this.task.finished = !this.task.finished;
+    this.taskService.putTask(this.task.id, this.task).subscribe((result) => {
       //Task is updated
     });
 
     this.crossed = {
-      "text-decoration": this.task.finished ? "line-through" : "none",
+      'text-decoration': this.task.finished ? 'line-through' : 'none',
     };
     this.updatedTaskEvent.emit();
+
+    this.setDateColor();
   }
 
-  editTask(){
+  editTask() {
     this.isEditable = !this.isEditable;
 
-    var date = moment(this.task.finishDate.toString(), "DD/MM/yyyy").format("yyyy-MM-DD");
+    var date = moment(this.task.finishDate.toString(), 'DD/MM/yyyy').format(
+      'yyyy-MM-DD'
+    );
 
     this.taskForm.setValue({
       task: this.task.task,
-      finishDate: date
+      finishDate: date,
     });
+
+    this.setDateColor();
   }
 
-  deleteTask(){
+  deleteTask() {
     var task = this.task;
-    this.deleteTaskEvent.emit({task});
+    this.deleteTaskEvent.emit({ task });
   }
 
   // reactive form
   taskForm = new FormGroup({
     task: new FormControl(''),
-    finishDate: new FormControl('')
+    finishDate: new FormControl(''),
   });
 
-  constructor(private taskService: TaskService) { }
+  constructor(private taskService: TaskService) {}
 
   ngOnInit(): void {
     this.crossed = {
-      "text-decoration": this.task.finished ? "line-through" : "none",
+      'text-decoration': this.task.finished ? 'line-through' : 'none',
     };
 
-    this.colorString = "color-" + this.color.substr(1);
+    this.colorString = 'color-' + this.color.substr(1);
 
+    this.setDateColor();
   }
 
   onSubmit(): void {
-    this.task.finishDate = moment(this.taskForm.get("finishDate")!.value).format("DD/MM/yyyy").toString();
-    this.task.task = this.taskForm.get("task")!.value;
-    this.taskService.putTask(this.task.id, this.task).subscribe(result => {
+    this.task.finishDate = moment(this.taskForm.get('finishDate')!.value)
+      .format('DD/MM/yyyy')
+      .toString();
+    this.task.task = this.taskForm.get('task')!.value;
+    this.taskService.putTask(this.task.id, this.task).subscribe((result) => {
       this.isEditable = false;
 
       var listId = this.task.listId;
-      this.newTask.emit({listId});
+      this.newTask.emit({ listId });
     });
+
+    this.setDateColor();
+  }
+
+  setDateColor() {
+    var passed =
+      moment(this.task.finishDate.toString(), 'DD/MM/yyyy') < moment() &&
+      !this.task.finished;
+    this.passed = {
+      color: passed ? 'red' : 'darkgrey',
+    };
   }
 }
