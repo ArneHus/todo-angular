ceeeb3d7966b86fb0b1de7344fba2890434357e3
Arne Hus <r0793284@student.thomasmore.be>
2021-11-23 12:30:36 +0100

    new pipe for lists only

---
+++ src/pipes/todo-count-list.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TodoCountListPipe } from './todo-count-list.pipe';
+
+describe('TodoCountListPipe', () => {
+  it('create an instance', () => {
+    const pipe = new TodoCountListPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
